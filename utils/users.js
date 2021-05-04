2d3710e71699e326fd99a61edc9a43af514ac537
calebbala <calebbala15@gmail.com>
2021-05-04 20:33:34 +0000

    finished

--- utils/users.js
+++ utils/users.js
@@ -3,8 +3,10 @@ const users = [];
 // Join user to chat
 function userJoin(id, username, room) {
   const user = { id, username, room };
-    users.push(user);
-    return user;
+
+  users.push(user);
+
+  return user;
 }
 
 // Get current user
